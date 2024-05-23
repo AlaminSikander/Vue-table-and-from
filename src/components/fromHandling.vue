0629e647f413df67eca1fe2da310dad3c36f84d6
AlaminSikander <alamin.6534@gmail.com>
2024-05-23 18:44:08 +0600

    from Submit

--- src/components/fromHandling.vue
+++ src/components/fromHandling.vue
@@ -4,7 +4,7 @@
       {{ JSON.stringify(fromValue, null, 2) }}
     </pre>
   </div>
-  <form action="">
+  <form action="" @submit="SubmitForm">
     <div>
       <label for="name">Name</label>
       <input type="text" id="name" v-model="fromValue.name">
@@ -23,7 +23,46 @@
         <option value="bangladesh">Bangladesh</option>
       </select>   
     </div>
-    
+    <div>
+      <label for="job-locaqtion">Job Locaqtion</label>
+      <select id="job-locaqtion" multiple v-model="fromValue.jobLocaqtion">
+        <option value="india">India</option>
+        <option value="vietnam">Vietnam</option>
+        <option value="bangladesh">Bangladesh</option>
+      </select>   
+    </div>
+
+    <div>
+      <input type="checkbox" id="remortlocation" v-model="fromValue.remortlocation" true-value="yes" false-value="no">
+      <label for="remoteLocation">Remote Location</label>
+    </div>
+
+    <div>
+      <label for="">Skill Set</label>
+      <input type="checkbox" id="html" value="html" v-model="fromValue.skillSet"/>
+      <label for="html">HTML</label>
+      <input type="checkbox" id="css" value="css" v-model="fromValue.skillSet"/>
+      <label for="css">CSS</label>
+      <input type="checkbox" id="js" value="js" v-model="fromValue.skillSet"/>
+      <label for="js">JS</label>
+    </div>
+
+    <div> 
+      <label>Years of Experience</label> 
+      <input type="radio" id="0-2" value="0-2" v-model="fromValue.yearsOfExperience"/> 
+      <label for="0-2">0-2</label> 
+
+      <input type="radio" id="3-5" value="3-5" v-model="fromValue.yearsOfExperience"/>
+      <label for="3-5">3-5</label>
+
+      <input type="radio" id="6-10" value="6-10" v-model="fromValue.yearsOfExperience"/>
+      <label for="6-10">6-10</label> 
+    </div>
+
+    <div>
+      <button>Submit</button>
+    </div>
+
   </form>
 </template>
 
@@ -35,9 +74,19 @@ export default {
       fromValue: {
         name: '',
         profileSummary: '',
-        country: ''
+        country: '',
+        jobLocaqtion: [],
+        remoteLocation: "no",
+        skillSet: [],
+        yearsOfExperience: ''
       }
       
+    }
+  },
+  methods: {
+    SubmitForm(event){
+      event.preventDefault();
+      console.log('From Values', this.fromValue);
     }
   }
 }
